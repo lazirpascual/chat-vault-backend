185870493cb0d3a5c4feac6030a447c818e08a96
Lazir Pascual <lazirpascual@gmail.com>
2021-08-05 22:01:52 -0400

    add route to create and update a post in posts route

--- routes/posts.js
+++ routes/posts.js
@@ -4,9 +4,21 @@ const Post = require("../models/Post");
 // create a post
 postsRouter.post("/", async (req, res) => {
   const newPost = new Post(req.body);
+  const savedPost = await newPost.save();
+  res.status(200).json(savedPost);
 });
 
 // update a post
+postsRouter.put("/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post.userId === req.body.userId) {
+    await post.updateOne({ $set: req.body });
+    res.status(200).json("The Post has been updated.");
+  } else {
+    res.status(403).json("Only the user can update this post.");
+  }
+});
+
 // delete a post
 // like a post
 // get a post
