1f72209edaf3fd50c77f4ccc37297ae2f69bedb8
Lazir Pascual <lazirpascual@gmail.com>
2021-08-05 20:40:36 -0400

    add route to delete user in users route

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const usersRouter = require("express").Router();
 
 // update user
 usersRouter.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       req.body.password = await bcrypt.hash(req.body.password, salt);
@@ -14,14 +14,28 @@ usersRouter.put("/:id", async (req, res) => {
     });
     res.status(200).json("Account has been updated");
   } else {
-    return res.status(403).json("Only a user can update their account");
+    return res
+      .status(403)
+      .json("Only the owner of this account can make an update request");
   }
 });
 
 // delete user
+usersRouter.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("Account has been deleted!");
+  } else {
+    return res
+      .status(403)
+      .json("Only the owner of this account can make a delete request");
+  }
+});
 
 // get a user
+
 // follow a user
+
 // unfollow a user
 
 module.exports = usersRouter;
