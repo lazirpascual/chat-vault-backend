cf72fa0e179457a2451bb937b4ac0ed9d34e4ee7
Lazir Pascual <lazirpascual@gmail.com>
2021-08-10 22:42:50 -0400

    fix file upload path

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const app = express();
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const upload = require("./utils/uploadFile");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const postsRouter = require("./routes/posts");
-const upload = require("./utils/uploadFile");
 const path = require("path");
 
 logger.info("connecting to", config.MONGODB_URI);
@@ -30,7 +30,7 @@ mongoose
     logger.error("error connecting to MongoDB:", error.message);
   });
 
-// by going to images path, go to directory images directory instead making a get request
+// when going to images path, go to directory images directory instead making a get request
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // middleware
@@ -41,7 +41,7 @@ app.use(helmet());
 app.use(morgan("tiny"));
 app.use(middleware.tokenExtractor);
 
-// upload file
+// upload image file
 app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
     return res.status(200).json("File uploded successfully");
--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
     cb(null, "public/images");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null, req.body.name);
   },
 });
 
