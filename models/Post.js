f160aa9a63163de55a94294c52e5dddad4c3d259
Lazir Pascual <lazirpascual@gmail.com>
2021-08-18 13:44:28 -0400

    add comments property to Post model

--- models/Post.js
+++ models/Post.js
@@ -17,6 +17,10 @@ const postSchema = mongoose.Schema(
       type: Array,
       default: [],
     },
+    comments: {
+      type: Array,
+      default: [],
+    },
   },
   { timestamps: true }
 );
